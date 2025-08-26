85052e404285e818806e696f20e75cef1fcb1f39
brunellaGiorda <brunellagiorda13@gmail.com>
2025-08-26 10:37:24 -0300

    menu hamburguesa

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("header nav");
+
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
